8898cf486b20617317c2bd65c5469b37b74f1cd9
JoeIt <josignaciot@gmail.com>
2023-03-15 18:18:47 -0300

    Extracting the questions

---
+++ src/pages/SurveyPage/SurvePage.styles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0 auto;
+  background-color: var(--secondary-color);
+  width: 70%;
+  height: 500px;
+
+  h1 {
+    padding: 2rem;
+    text-align: center;
+    background-color: inherit;
+    color: var(--black);
+    font-size: 3rem;
+  }
+`;
--- src/pages/SurveyPage/SurveyPage.jsx
+++ src/pages/SurveyPage/SurveyPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getQuestionsItems } from '../../features/questionsAnswers/questionsAnswers.js';
+import { Container } from './SurvePage.styles.jsx';
 
 function SurveyPage() {
   const { questions } = useSelector(state => state.questionsAnswers);
@@ -12,7 +13,30 @@ function SurveyPage() {
 
   console.log(questions);
 
-  return <div>SurveyPage</div>;
+  return (
+    <Container>
+      <h1>Challenge Greydive</h1>
+      <form>
+        {questions?.map(question => {
+          switch (question.type) {
+            case 'text':
+              return <p>{question.label}</p>;
+            case 'email':
+              return <p>{question.label}</p>;
+            case 'date':
+              return <p>{question.label}</p>;
+            case 'select':
+              return <p>{question.label}</p>;
+            case 'checkbox':
+              return <p>{question.label}</p>;
+            default:
+              return null;
+          }
+        })}
+        <button type="submit">Enviar</button>
+      </form>
+    </Container>
+  );
 }
 
 export default SurveyPage;
